007f39a0eedff20ded6b2b4d6c1f0402542f8909
Vuchke <vuchke@gmail.com>
2020-08-27 19:20:21 +0200

    vue menu & img compress

--- app.js
+++ app.js
@@ -1,39 +1,5 @@
 
 
-
-
-
-
-
-
-
-//Food Menu Tabs
-
-// Get the element with id="defaultOpen" and click on it making it open
-document.getElementById("defaultOpen").click();
-
-function openMenu(event, menuName){
-  // Declare all variables
-  var i, tabContent, tabLinks;
-
-  // Get all elements with class='tabContent' and hide them
-  tabContent = document.getElementsByClassName('tabcontent')
-  for (let i = 0; i < tabContent.length; i++) {
-     tabContent[i].style.display= 'none';    
-  }
-
-   // Get all elements with class="tablinks" and remove the class "active"
-   tabLinks = document.getElementsByClassName("tablinks");
-   for (i = 0; i < tabLinks.length; i++) {
-     tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-   }
-
-  // Show current tab, and add an active class
-  document.getElementById(menuName).style.display = 'block'  
-  event.currentTarget.className += " active"
-};
-
-
 // Map 
 // Initialize and add the map
 function initMap(){
