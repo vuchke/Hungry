b5a693456e1b8ba24d370b949b50ba8260aa3afe
Vuchke <vuchke@gmail.com>
2020-08-31 15:12:40 +0200

    animation

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
+// // animations
+// const faders = document.querySelectorAll(".fade-in");
+// const sliders = document.querySelectorAll('.slide-in')
+const animates = document.querySelectorAll('.animate')
+const appearOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px -350px 0px'
+
+};
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add('appear')
+      appearOnScroll.unobserve(entry.target)
+    }
+  });
+}, appearOptions);
+
+animates.forEach(animate => {
+  appearOnScroll.observe(animate)
+})
+
+// faders.forEach(fader => {
+//   appearOnScroll.observe(fader)
+// })
+// sliders.forEach(slider => {
+//   appearOnScroll.observe(slider)
+// })
 
 
-// Map 
+// Map
 // Initialize and add the map
-function initMap(){
+function initMap() {
   // The location of Uluru
-  var subotica = {lat: 46.100158, lng: 19.664203};
+  var subotica = { lat: 46.100158, lng: 19.664203 };
   // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 10, center: subotica});
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: subotica,
+  });
   // The marker, positioned at subotica
-  var marker = new google.maps.Marker({position: subotica, map: map});
+  var marker = new google.maps.Marker({ position: subotica, map: map });
 }
